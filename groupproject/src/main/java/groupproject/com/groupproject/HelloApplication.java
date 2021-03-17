1cd22fda5818254cc0e11c922e15e63334e5299c
darleneagbayani <31275691+darleneagbayani@users.noreply.github.com>
2021-03-17 09:50:27 -1000

    Delete groupproject directory

--- groupproject/src/main/java/groupproject/com/groupproject/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package groupproject.com.groupproject;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
