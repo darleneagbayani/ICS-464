d168525b22088506091c8e9f55e239d7401a03fc
Michael Arcangel <mgarcang@hawaii.edu>
2021-03-22 20:09:01 -1000

    About Page Mock up
    Styles, placeholders tentatively implemented

--- assets/About/index.js
+++ assets/About/index.js
@@ -1,10 +1,20 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
+import styles from './styles';
 
 function About() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>About Screen</Text>
+      <View style={styles.header}>
+        <Text style={styles.text1}>About the App</Text>
+        <Text style={styles.text2}>Purpose</Text>
+        <Text style={styles.text3}>Purpose goes here</Text>
+        <Text style={styles.text2}>Features</Text>
+        <Text style={styles.text3}>Find trending news updates and available testing sites
+          conveniently packed into one lightweight application. Stay alert
+          with notifications about reported cases in the State of Hawaii.
+        </Text>
+        <Text style={styles.text2}>Helping Hawaii</Text>
+        <Text style={styles.text3}>Stuff goes here</Text>
       </View>
     );
   }
---
+++ assets/About/styles.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    header: {
+        width: '100%',
+        position: 'absolute',
+        top: 60,
+        zIndex: 100,
+        justifyContent: 'center',
+        paddingHorizontal: 20
+    },
+    text1: {
+        fontSize: 40,
+        fontWeight: 'bold'
+    },
+    text2: {
+        fontSize: 22,
+        fontWeight: '500'
+    },
+    text3: {
+        fontSize: 18,
+        fontWeight: '300'
+    }
+});
+
+export default styles;
