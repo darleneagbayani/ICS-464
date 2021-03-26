37691e292597f43df9a2aa7d45a6baea68111a66
Michael Arcangel <mgarcang@hawaii.edu>
2021-03-26 12:54:16 -1000

    Updated styles

--- assets/About/index.js
+++ assets/About/index.js
@@ -1,40 +1,30 @@
 import React, { Component } from 'react';
-import {
-  Switch,
-  ScrollView,
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-} from 'react-native';
-import Constants from 'expo-constants';
+import { Switch, ScrollView, Text, View, TouchableOpacity, } from 'react-native';
+import styles from './styles';
 import * as Animatable from 'react-native-animatable';
 import Collapsible from 'react-native-collapsible';
 import Accordion from 'react-native-collapsible/Accordion';
 
-const BACON_IPSUM =
-  'Bacon ipsum dolor amet chuck turducken landjaeger tongue spare ribs. Picanha beef prosciutto meatball turkey shoulder shank salami cupim doner jowl pork belly cow. Chicken shankle rump swine tail frankfurter meatloaf ground round flank ham hock tongue shank andouille boudin brisket. ';
-
 const CONTENT = [
   {
-    title: 'First',
-    content: BACON_IPSUM,
+    title: 'Purpose',
+    content: 'test',
   },
   {
-    title: 'Second',
-    content: BACON_IPSUM,
+    title: 'Features',
+    content: 'test',
   },
   {
     title: 'Third',
-    content: BACON_IPSUM,
+    content: 'test',
   },
   {
     title: 'Fourth',
-    content: BACON_IPSUM,
+    content: 'test',
   },
   {
     title: 'Fifth',
-    content: BACON_IPSUM,
+    content: 'test',
   },
 ];
 
@@ -101,7 +91,7 @@ export default class App extends Component {
     return (
       <View style={styles.container}>
         <ScrollView contentContainerStyle={{ paddingTop: 30 }}>
-          <Text style={styles.title}>Accordion Example</Text>
+          <Text style={styles.title}>About the App</Text>
 
           <View style={styles.multipleToggle}>
             <Text style={styles.multipleToggle__title}>Multiple Select?</Text>
@@ -161,63 +151,3 @@ export default class App extends Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F5FCFF',
-    paddingTop: Constants.statusBarHeight,
-  },
-  title: {
-    textAlign: 'center',
-    fontSize: 22,
-    fontWeight: '300',
-    marginBottom: 20,
-  },
-  header: {
-    backgroundColor: '#F5FCFF',
-    padding: 10,
-  },
-  headerText: {
-    textAlign: 'center',
-    fontSize: 16,
-    fontWeight: '500',
-  },
-  content: {
-    padding: 20,
-    backgroundColor: '#fff',
-  },
-  active: {
-    backgroundColor: 'rgba(255,255,255,1)',
-  },
-  inactive: {
-    backgroundColor: 'rgba(245,252,255,1)',
-  },
-  selectors: {
-    marginBottom: 10,
-    flexDirection: 'row',
-    justifyContent: 'center',
-  },
-  selector: {
-    backgroundColor: '#F5FCFF',
-    padding: 10,
-  },
-  activeSelector: {
-    fontWeight: 'bold',
-  },
-  selectTitle: {
-    fontSize: 14,
-    fontWeight: '500',
-    padding: 10,
-  },
-  multipleToggle: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    marginVertical: 30,
-    alignItems: 'center',
-  },
-  multipleToggle__title: {
-    fontSize: 16,
-    marginRight: 8,
-  },
-});
--- assets/About/styles.js
+++ assets/About/styles.js
@@ -1,26 +1,63 @@
 import { StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
 
 const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#F5FCFF',
+      paddingTop: Constants.statusBarHeight,
+    },
+    title: {
+      textAlign: 'center',
+      fontSize: 22,
+      fontWeight: '300',
+      marginBottom: 20,
+    },
     header: {
-        width: '100%',
-        //position: 'absolute',
-        top: 60,
-        zIndex: 100,
-        justifyContent: 'center',
-        paddingHorizontal: 20
+      backgroundColor: '#F5FCFF',
+      padding: 10,
     },
-    text1: {
-        fontSize: 40,
-        fontWeight: 'bold'
+    headerText: {
+      textAlign: 'center',
+      fontSize: 16,
+      fontWeight: '500',
     },
-    text2: {
-        fontSize: 22,
-        fontWeight: '500'
+    content: {
+      padding: 20,
+      backgroundColor: '#fff',
     },
-    text3: {
-        fontSize: 18,
-        fontWeight: '300'
-    }
-});
-
-export default styles;
+    active: {
+      backgroundColor: 'rgba(255,255,255,1)',
+    },
+    inactive: {
+      backgroundColor: 'rgba(245,252,255,1)',
+    },
+    selectors: {
+      marginBottom: 10,
+      flexDirection: 'row',
+      justifyContent: 'center',
+    },
+    selector: {
+      backgroundColor: '#F5FCFF',
+      padding: 10,
+    },
+    activeSelector: {
+      fontWeight: 'bold',
+    },
+    selectTitle: {
+      fontSize: 14,
+      fontWeight: '500',
+      padding: 10,
+    },
+    multipleToggle: {
+      flexDirection: 'row',
+      justifyContent: 'center',
+      marginVertical: 30,
+      alignItems: 'center',
+    },
+    multipleToggle__title: {
+      fontSize: 16,
+      marginRight: 8,
+    },
+  });
+  export default styles;
