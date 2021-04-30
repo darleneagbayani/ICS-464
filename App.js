c2d5cc94776dff1cde8024a9d4483d199a58ca6c
Dre Cardinalli <35351020+cardinalli-uh@users.noreply.github.com>
2021-04-30 08:15:18 -1000

    Update App.js

--- App.js
+++ App.js
@@ -4,32 +4,34 @@ import MyTabs from './assets/Navigation';
 import {Platform} from ‘react-native’; 
 
 import {Platform} from ‘react-native’;componentWillMount(){
-   // get expo push token
-  const token = await Expo.Notifications.getExpoPushTokenAsync();fetch('https://exp.host/--/api/v2/push/send', {       
-         method: 'POST', 
-         headers: {
-               Accept: 'application/json',  
-              'Content-Type': 'application/json', 
-              'accept-encoding': 'gzip, deflate',   
-              'host': 'exp.host'      
-          }, 
-        body: JSON.stringify({                 
-              to: token,                        
-              title: 'New Notification',                  
-              body: 'The notification worked!',             
-              priority: "high",            
-              sound:"default",              
-              channelId:"default",   
-                  }),        
-      }).then((response) => response.json())   
-               .then((responseJson) => {  })
-                      .catch((error) => { console.log(error) });
+	// get expo push token
+	const token = await Expo.Notifications.getExpoPushTokenAsync();
+   
+fetch('https://exp.host/--/api/v2/push/send', {       
+	method: 'POST', 
+	headers: {
+		Accept: 'application/json',  
+		'Content-Type': 'application/json', 
+		'accept-encoding': 'gzip, deflate',   
+		'host': 'exp.host'      
+	}, 
+	body: JSON.stringify({                 
+		to: token,                        
+		title: 'New Notification',                  
+		body: 'The notification worked!',             
+		priority: "high",            
+		sound:"default",              
+		channelId:"default",   
+	}),        
+	}).then((response) => response.json())   
+	.then((responseJson) => {  })
+		.catch((error) => { console.log(error) });
 }
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <MyTabs />
-    </NavigationContainer>
-  );
+	return (
+		<NavigationContainer>
+		<MyTabs />
+		</NavigationContainer>
+	);
 }
